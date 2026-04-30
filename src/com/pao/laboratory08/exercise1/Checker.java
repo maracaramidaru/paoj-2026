2801c65de60f2e4f8e41492c9114c155bf80bc99
maracaramidaru <maracaramidaru2@gmail.com>
2026-04-30 16:02:02 +0300

    Salvare progres înainte de reconfigurare

--- src/com/pao/laboratory08/exercise1/Checker.java
+++ src/com/pao/laboratory08/exercise1/Checker.java
@@ -1,10 +1,10 @@
-package com.pao.laboratory08.exercise1;
-
-import com.pao.test.IOTest;
-
-public class Checker {
-    public static void main(String[] args) {
-        IOTest.runParts("C:\\Users\\marac\\IdeaProjects\\paoj1\\paoj-2026\\src\\com\\pao\\laboratory08\\exercise1\\tests", Main::main);
-    }
-}
-
+//package com.pao.laboratory08.exercise1;
+//
+//import com.pao.test.IOTest;
+//
+//public class Checker {
+//    public static void main(String[] args) {
+//        IOTest.runParts("C:\\Users\\marac\\IdeaProjects\\paoj1\\paoj-2026\\src\\com\\pao\\laboratory08\\exercise1\\tests", Main::main);
+//    }
+//}
+//
